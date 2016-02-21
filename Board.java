204e940ad402e76f4562eafd8b1c4357cc012ba3
nmpappas77 <pappasn@mail.gvsu.edu>
2016-02-21 13:00:39 -0500

    Updated verion of the GUI Panel

--- Board.java
+++ Board.java
@@ -11,20 +11,25 @@ public class Board extends JPanel
 {
     private SequenceGame game;
     private JFrame frame;
-    private JPanel center, left, right;
+    private JPanel center, left, right, bottom;
     private ButtonListener listener;
     private JButton[][] gameBoard = new JButton[8][8];
     private JButton[] player1Hand = new JButton[5];
     private JButton[] player2Hand = new JButton[5];
-    private Dimension dim = new Dimension(40,40);
+    private Dimension dim = new Dimension(10,10);
     
     
     private Space[][] iBoard;
     
     private String[] iDeck;
     
+    private String[] iPlayerOne;
+    
+    private String[] iPlayerTwo;
+    
     
     public Board(){
+    	   	
         listener = new ButtonListener();
         
         //create a game to be played
@@ -34,53 +39,65 @@ public class Board extends JPanel
         center = new JPanel();
         left = new JPanel();
         right = new JPanel();
+        bottom = new JPanel();
+        
+        setLayout (new BorderLayout ());
+        
+		add(center, BorderLayout.CENTER);
+		add(bottom, BorderLayout.SOUTH);
+		//add(right, BorderLayout.EAST);
         
       //create board grid
-      		center.setLayout(new GridLayout(8,8,5,7));
+      		center.setLayout(new GridLayout(8,8,0,0));
+      		//right.setLayout(new GridLayout(5,1,0,0));
+      		bottom.setLayout(new GridLayout(5,1,0,0));
       		
       		constructBoard(center, listener);
       		
       		showBoard();
       		
-      		setLayout (new BorderLayout ());
-    		add(center, BorderLayout.CENTER);
-    		add(left, BorderLayout.WEST);
-    		add(right, BorderLayout.EAST);
+      //create hands
+      		
+      		
+      		//constructHand(right, player1Hand);
+      		constructHand(bottom, player2Hand);
+      		
+      		
     }
     
     public void constructBoard(JPanel panel, ButtonListener listener){
                
         for(int row = 0; row < 8; row++){
-            for(int col = 0; col < 8; col++)
-            {
+            for(int col = 0; col < 8; col++){
+            
             	Border thickBorder = new LineBorder(Color.black, 2);
 				
             	//creates buttons based on board, adds listener and adds to panel
 				gameBoard[row][col] = new JButton("");
-				gameBoard[row][col].setPreferredSize(dim);
+				gameBoard[row][col].setPreferredSize(new Dimension(10,10));
+				//gameBoard[row][col].setSize(20,20);
 				gameBoard[row][col].setBorder(thickBorder);
 				
 				gameBoard[row][col].addActionListener(listener);
 				panel.add(gameBoard[row][col]);
-        }
-    }
-
-    for( int x = 0; x == 4; x++){
-        JButton handButton = new JButton();
-        
-        player1Hand[x] = handButton;
+            }
+        }   
+   }
     
-        right.add(handButton);
-    }
-    
-    for (int y = 0; y == 4; y++)
-    {
-        JButton handButton = new JButton();
-        
-        player2Hand[y] = handButton;
-        
-        left.add(handButton);
-    }
+    public void constructHand(JPanel panel, JButton[] hand){
+    	for(int i = 0; i < 5; i++){
+    		JButton[] phand;
+    		
+    		phand = hand;
+    		
+    		Border thickBorder = new LineBorder(Color.black, 2);
+    		
+    		phand[i] = new JButton("");
+    		phand[i].setPreferredSize(dim);
+    		phand[i].setBorder(thickBorder);
+    		
+    		panel.add(phand[i]);
+    	}
     }
     
     private void showBoard(){
@@ -94,7 +111,7 @@ public class Board extends JPanel
 				
 				gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
 	}
-    }
+   }
     
     private class ButtonListener implements ActionListener{
 		
