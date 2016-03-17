d8164848d249d2236a0d6f100f6b56d7277ac546
nmpappas77 <pappasn@mail.gvsu.edu>
2016-03-17 15:45:28 -0400

    Board with updated spaces with icons

--- Board.java
+++ Board.java
@@ -1,11 +1,15 @@
 package package1;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Board extends JPanel
 {
@@ -20,7 +24,8 @@ public class Board extends JPanel
     
     private int counter;
     
-    
+    private ImageIcon kingheart, aceheart, queenheart, jackheart, aceclub, kingclub, queenclub, jackclub, 
+    	acediamond, kingdiamond, queendiamond,jackdiamond, acespade, kingspade, queenspade, jackspade; 
     
     private Space[][] iBoard;
     
@@ -32,7 +37,27 @@ public class Board extends JPanel
     
     
     public Board(){
-    	   	
+    	aceheart = new ImageIcon(this.getClass().getResource("/aceheart.png"));
+    	kingheart = new ImageIcon(this.getClass().getResource("/kingheart.png"));
+    	queenheart = new ImageIcon(this.getClass().getResource("/queenheart.png"));
+    	jackheart = new ImageIcon(this.getClass().getResource("/jackheart.png"));
+    	
+    	acediamond = new ImageIcon(this.getClass().getResource("/acediamond.png"));
+    	kingdiamond = new ImageIcon(this.getClass().getResource("/kingdiamond.png"));
+    	queendiamond = new ImageIcon(this.getClass().getResource("/queendiamond.png"));
+    	jackdiamond = new ImageIcon(this.getClass().getResource("/jackdiamond.png"));
+    	
+    	acespade = new ImageIcon(this.getClass().getResource("/acespades.png"));
+    	kingspade = new ImageIcon(this.getClass().getResource("/kingspades.png"));
+    	queenspade = new ImageIcon(this.getClass().getResource("/queenspades.png"));
+    	jackspade = new ImageIcon(this.getClass().getResource("/jackspades.png"));
+    	
+    	aceclub = new ImageIcon(this.getClass().getResource("/aceclubs.png"));
+    	kingclub = new ImageIcon(this.getClass().getResource("/kingclubs.png"));
+    	queenclub = new ImageIcon(this.getClass().getResource("/queenclubs.png"));
+    	jackclub = new ImageIcon(this.getClass().getResource("/jackclubs.png"));
+
+    	
         listener = new ButtonListener();
         
         //create a game to be played
@@ -127,12 +152,46 @@ public class Board extends JPanel
     
     private void showBoard(){
 		iBoard = game.getBoard();
+		
+		System.out.println(kingheart.getIconHeight());
 				
 		//set button text on panel
 		for (int row = 0; row < 8; row++)
 			for (int col = 0; col < 8; col++){
+				//gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
 				
-				gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
+				if(iBoard[row][col].getCardSpace().equals("K of Hearts"))
+				gameBoard[row][col].setIcon(kingheart);				
+				else if(iBoard[row][col].getCardSpace().equals("A of Hearts"))
+					gameBoard[row][col].setIcon(aceheart);
+				else if(iBoard[row][col].getCardSpace().equals("Q of Hearts"))
+					gameBoard[row][col].setIcon(queenheart);
+				else if(iBoard[row][col].getCardSpace().equals("J of Hearts"))
+					gameBoard[row][col].setIcon(jackheart);
+				else if(iBoard[row][col].getCardSpace().equals("A of Diamonds"))
+					gameBoard[row][col].setIcon(acediamond);
+				else if(iBoard[row][col].getCardSpace().equals("K of Diamonds"))
+					gameBoard[row][col].setIcon(kingdiamond);
+				else if(iBoard[row][col].getCardSpace().equals("Q of Diamonds"))
+					gameBoard[row][col].setIcon(queendiamond);
+				else if(iBoard[row][col].getCardSpace().equals("J of Diamonds"))
+					gameBoard[row][col].setIcon(jackdiamond);
+				else if(iBoard[row][col].getCardSpace().equals("A of Spades"))
+					gameBoard[row][col].setIcon(acespade);
+				else if(iBoard[row][col].getCardSpace().equals("K of Spades"))
+					gameBoard[row][col].setIcon(kingspade);
+				else if(iBoard[row][col].getCardSpace().equals("Q of Spades"))
+					gameBoard[row][col].setIcon(queenspade);
+				else if(iBoard[row][col].getCardSpace().equals("J of Spades"))
+					gameBoard[row][col].setIcon(jackspade);
+				else if(iBoard[row][col].getCardSpace().equals("A of Clubs"))
+					gameBoard[row][col].setIcon(aceclub);
+				else if(iBoard[row][col].getCardSpace().equals("K of Clubs"))
+					gameBoard[row][col].setIcon(kingclub);
+				else if(iBoard[row][col].getCardSpace().equals("Q of Clubs"))
+					gameBoard[row][col].setIcon(queenclub);
+				else
+					gameBoard[row][col].setIcon(jackclub);
 	}
    }
     
@@ -148,9 +207,11 @@ public class Board extends JPanel
                                     game.select(row,col);
                                     
                                     iBoard = game.getBoard();
-                                    gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
+                                   
                                     if(game.getValid() == true){
                                     	gameBoard[row][col].setBackground(Color.GREEN);
+                                    	gameBoard[row][col].setIcon(null);
+                                    	gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
                                     	displayHand2();
                                     }
                                     game.setValid(false);
@@ -159,10 +220,12 @@ public class Board extends JPanel
                                       game.select(row,col);
                                       
                                       iBoard = game.getBoard();
-                                      gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
+                                      
                                       if(game.getValid() == true){
                                       	displayHand1();
                                       	gameBoard[row][col].setBackground(Color.ORANGE);
+                                    	gameBoard[row][col].setIcon(null);
+                                      	gameBoard[row][col].setText(iBoard[row][col].getCardSpace());
                                       }
                                       game.setValid(false);
                                }
