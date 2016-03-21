0af95b342f3831ca031919772f52ca4fc5ec2f7b
Casey VanHoof <vanhoofc@mail.gvsu.edu>
2016-03-21 15:42:29 -0400

    Update WinMethod.java

--- WinMethod.java
+++ WinMethod.java
@@ -5,74 +5,71 @@
 	 */
 	
 	private boolean gameWon() {
-
+	
 		//check for horizontal win
-		int counter=0;
-		for (int row = 0; row < 8; row++){
-			for(int col = 1; col < 8; col++){
-				if (board [row][col] == board[row][col-1] && board [row][col] == board[row][col-2] && 
-						board [row][col] == board[row][col-3])
+		
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+			if(board[i][j].getCardSpace() == playerOne[i].toString()){
+				if (board[i][j].getCardSpace() == board[i-1][j].getCardSpace()){
 					counter++;
-				
-		if (counter >= 4){
-			return true;
-		   }
+				}
+			}
+			if (counter >= 4)
+				return true;
 		}
 	}
-		//check vertical win
-		int counter1 = 0;
-		for (int col = 0; col < 8; col++){
-			for (int row1 = 1; row1 < 8; row1++){
-				if (board[row1][col] == board[row1-1][col] && board[row1][col] == board[row1-2][col] && 
-						board[row1][col] == board[row1-3][col])
-					counter1++;
-			
-				if (counter1 >= 4)
-					return true;
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+			if(board[i][j].getCardSpace() == playerTwo[i].toString()){
+				if (board[i][j].getCardSpace() == board[i][j-1].getCardSpace())
+				counter++;
 			}
+			if (counter >= 4)
+				return true;
 		}
+	}
 		
-		//check for diagonal from top-left to bottom-right
-		int count = 0;
-		for (int col1 = 0; col1 < 8; ++col1) {
-            for (int row1 = 1; row1 < 8; ++row1) {
-                if (col1 + row1 >= 8) break;
-                if (board[row1][col1] == board[row1-1][col1 + row1 - 1] && board[row1][col1] == board[row1][col1+row1])
-                    ++count;
-       
-                if (count >= 4) 
-                	return true;
-            }
-        }
-        
-        // There are diagonals, that starts on left of each row
-        int count1 = 0; 
-		for (int row1 = 0; row1 < 8; ++row1) {
-            for (int col1 = 1; col1 < 8; ++col1) {
-                if (col1 + row1 >= 7) break;
-                if (board[row1][col1] == board[row1+col1 - 1][col1 - 1] && board[row1][col1] == board[row1 + col1][col1])
-                    ++count1;
-             
-                if (count1 >= 4) 
-                	return true;
-            }
-        }
-        
-        //check diagonal top-right to bottom-left
-        int counter2 = 0;
-		for (int col1 = 0; col1 < 8; ++col1) {
-            for (int row1 = 1; row1 < 8; ++row1) {
-                if (col1 - row1 < 0) break;
-                if (board[row1][col1] == board[row1 - 1][col1 - row1 + 1] && board[row1][col1] == board[row1][col1-row1])
-                    counter2++;
-        
-                if (counter2 == 4) 
-                	return true;
-                
-            }	
-        }
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+			if(board[i][j].getCardSpace() == playerTwo[i].toString()){
+				if (board[i][j].getCardSpace() == board[i-1][j-1].getCardSpace())
+				counter++;
+			}
+			if (counter >= 4)
+				return true;
+		}
+	}
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+			if(board[i][j].getCardSpace() == playerTwo[i].toString()){
+				if (board[i][j].getCardSpace() == board[i+1][j+1].getCardSpace())
+				counter++;
+			}
+			if (counter >= 4)
+				return true;
+		}
+	}
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+			if(board[i][j].getCardSpace() == playerTwo[i].toString()){
+				if (board[i][j].getCardSpace() == board[i+1][j-1].getCardSpace())
+				counter++;
+			}
+			if (counter >= 4)
+				return true;
+		}
+	}
 		
-		
-		
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+			if(board[i][j].getCardSpace() == playerTwo[i].toString()){
+				if (board[i][j].getCardSpace() == board[i-1][j+1].getCardSpace())
+				counter++;
+			}
+			if (counter >= 4)
+				return true;
+		}
+	}
 		return false;
-	}	
+}
