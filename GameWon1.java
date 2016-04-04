e036f5646168dc7427da84f67babc53a7a4ecb6a
Casey VanHoof <vanhoofc@mail.gvsu.edu>
2016-04-04 15:20:20 -0400

    update on game won
    game won with diagonal checks and checks in col=4 and row=5/6/7

--- GameWon1.java
+++ GameWon1.java
@@ -183,4 +183,50 @@
 						}
 					}
 				}
+				//check final two spots in diagonal (up-right check)
+				for(int row = 0; row < 8; row++){
+					for(int col = 0; col < 8; col++){
+						if(board[row][col].getCardSpace().equals("Player One") || 
+								board[row][col].getCardSpace().equals("Player Two")){
+							String play = board[row][col].getCardSpace();
+							tick++;
+							
+							if(col == 4)
+								if(row == 5 || row == 6 || row == 7){
+								tick = 0;
+								continue;
+								}
+								else{
+									if(board[row + 1][col + 1].getCardSpace().equals(play))
+										tick++;
+									else{
+										tick = 0;
+									}
+									
+									if(board[row + 2][col + 2].getCardSpace().equals(play))
+										tick++;
+									else{
+										tick = 0;
+									}
+									
+									if(board[row + 3][col + 3].getCardSpace().equals(play))
+										tick++;
+									else{
+										tick = 0;
+									}
+									
+									if(tick == 4){
+										if(counter % 2 == 1){
+											oneWin = true;								
+											System.out.println("Player One Wins");
+										}
+										else{
+											twoWin = true;
+											System.out.println("Player Two Wins");
+										}
+									}
+								}
+							}
+						}
+					}
 	}
