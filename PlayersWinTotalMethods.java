01134eefbc12a0556c8082882fc38581c36f834b
Casey VanHoof <vanhoofc@mail.gvsu.edu>
2016-04-04 15:51:53 -0400

    win totals for each player
    in SequenceGame.java Class

---
+++ PlayersWinTotalMethods.java
@@ -0,0 +1,15 @@
+	public int Player1WinTotal(){
+		int counter1 = 0;
+		if(oneWin == true){
+			counter1++;
+		}
+		return counter1;
+	}
+
+	public int Player2WinTotal(){
+		int counter2 = 0;
+		if(twoWin == true){
+			counter2++;
+		}
+		return counter2;
+	}
